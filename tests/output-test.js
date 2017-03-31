2ff5fd55cd24fa88413ae1bc854cfa415a469d24
Jason Mitchell <jason.mitchell.w@gmail.com>
2017-03-31 12:53:14 -0700

    Removing destructuring since not supported in 4.0

--- tests/output-test.js
+++ tests/output-test.js
@@ -25,17 +25,17 @@ function build(pluginOptions) {
 
 describe('cldr data extraction', function() {
   it('filenames should be the locale', function() {
-    return build().then(({ directory }) => {
-      let ls = walkSync(directory);
+    return build().then(result => {
+      let ls = walkSync(result.directory);
       assert.ok(~ls.indexOf('en.js'));
       assert.ok(~ls.indexOf('zh.js'));
     });
   });
 
   it('should have object values for each locale key', function() {
-    return build().then(({ directory }) => {
-      let en = require(path.join(directory, 'en.js'));
-      let zh = require(path.join(directory, 'zh.js'));
+    return build().then(result => {
+      let en = require(path.join(result.directory, 'en.js'));
+      let zh = require(path.join(result.directory, 'zh.js'));
       assert.equal(typeof en, 'object');
       assert.equal(typeof zh, 'object');
     });
@@ -45,9 +45,9 @@ describe('cldr data extraction', function() {
     return build({
       locales: ['EN-ca', 'fr-CA'],
       pluralRules: true
-    }).then(({ directory }) => {
-      let ls = walkSync(directory);
-      let en = require(path.join(directory, 'en.js'));
+    }).then(result => {
+      let ls = walkSync(result.directory);
+      let en = require(path.join(result.directory, 'en.js'));
       let enCA = en.find(o => o.locale === 'en-CA');
       assert.equal(ls.length, 2, 'contains only fr and en modules');
       assert.ok(enCA);
@@ -66,9 +66,9 @@ describe('cldr data extraction', function() {
     return build({
       locales: ['en_CA', 'fr_ca'],
       pluralRules: true
-    }).then(({ directory }) => {
-      let ls = walkSync(directory);
-      let en = require(path.join(directory, 'en.js'));
+    }).then(result => {
+      let ls = walkSync(result.directory);
+      let en = require(path.join(result.directory, 'en.js'));
       let enCA = en.find(o => o.locale === 'en-CA');
       assert.equal(ls.length, 2, 'contains only fr and en modules');
       assert.ok(enCA);
@@ -79,17 +79,17 @@ describe('cldr data extraction', function() {
     return build({
       locales: ['en-ca', 'fr-ca'],
       pluralRules: true
-    }).then(({ directory }) => {
-      let ls = walkSync(directory);
-      let en = require(path.join(directory, 'en.js'));
+    }).then(result => {
+      let ls = walkSync(result.directory);
+      let en = require(path.join(result.directory, 'en.js'));
       assert.equal(ls.length, 2, 'contains only fr and en modules');
       assert.ok(en.find(locale => typeof locale.pluralRuleFunction === 'function'));
     });
   });
 
   it('should not include pluralRuleFunction function when pluralRules disabled', function() {
-    return build().then(({ directory }) => {
-      let en = require(path.join(directory, 'en.js'));
+    return build().then(result => {
+      let en = require(path.join(result.directory, 'en.js'));
       assert.equal(typeof en[0].pluralRuleFunction, 'undefined');
     });
   });
